babe61c86573151f0c950dba8e1962d026385899
Isaac Ashwin <ashiswin@gmail.com>
2018-02-05 20:39:30 +0800

    Made nav appropriate

--- manage.php
+++ manage.php
@@ -35,29 +35,15 @@
 		</style>
 	</head>
 	<body>
-		<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
-			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<a class="navbar-brand" href="/"><img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
- <span style="font-family: 'Ubuntu', Arial, sans-serif">KodYaC</span></a>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav">
-					<li class="nav-item"><span class="nav-link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome <?php echo $company["name"]; ?></span></li>
-				</ul>
-		
-				<ul class="navbar-nav ml-auto">
-					<li id="navAircrafts" class="nav-item"><a href="aircrafts.php" class="nav-link">Aircrafts</a></li>
-					<li id="navDefects" class="nav-item"><a href="defects.php" class="nav-link">Defects</a></li>
-					<li id="navMaintenance" class="nav-item"><a href="maintenance.php" class="nav-link">Maintenance</a></li>
-					<li id="navStatistics" class="nav-item"><a href="statistics.php" class="nav-link">Statistics</a></li>
-					<li id="navTechnicians" class="nav-item"><a href="technicians.php" class="nav-link">Technicians</a></li>
-					<li id="navLogout" class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+		<?php require_once 'nav.php' ?>
 		<div class="bg-inverse">
 			<h1 style="font-family: 'Martel', Times New Roman, serif; font-weight: bold; text-align: center; padding: 5vh; color: #FFFFFF; font-size: 3em">Dashboard</h1>
 		</div>
 	</body>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		$("#navDashboard").addClass('active');
+	</script>
 </html>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="/"><img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+<span style="font-family: 'Ubuntu', Arial, sans-serif">KodYaC</span></a>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav">
+			<li class="nav-item"><span class="nav-link">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome <?php echo $company["name"]; ?></span></li>
+		</ul>
+
+		<ul class="navbar-nav ml-auto">
+			<li id="navDashboard" class="nav-item"><a href="manage.php" class="nav-link">Dashboard</a></li>
+			<li id="navStatistics" class="nav-item"><a href="statistics.php" class="nav-link">Statistics</a></li>
+			<li id="navAPIKeys" class="nav-item"><a href="apikeys.php" class="nav-link">API Keys</a></li>
+			<li id="navLogout" class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
+		</ul>
+	</div>
+</nav>
