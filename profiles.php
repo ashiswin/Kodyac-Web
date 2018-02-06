85f1c353be96da7fb3716e62869939913ace860a
Isaac Ashwin <ashiswin@gmail.com>
2018-02-06 11:56:21 +0800

    Added profile table with latest FA

--- profiles.php
+++ profiles.php
@@ -10,20 +10,16 @@
 	
 	require_once 'scripts/utils/database.php';
 	require_once 'scripts/connectors/CompanyConnector.php';
-	require_once 'scripts/connectors/LinkConnector.php';
 	
 	$CompanyConnector = new CompanyConnector($conn);
-	$LinkConnector = new LinkConnector($conn);
-	
 	$company = $CompanyConnector->select($_SESSION['companyId']);
-	$noRequested = count($LinkConnector->selectByCompany($_SESSION['companyId']));
-	$noCompleted = count($LinkConnector->selectByStatus("completed"));
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>KodYaC - Dashboard</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<style type="text/css">
 			html, body {
 				margin: 0;
@@ -62,6 +58,18 @@
 					<a class="nav-link" id="tabCancelled" href="#">Cancelled</a>
 				</li>
 			</ul>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Profile ID</th>
+						<th>Profile Name</th>
+						<th><i class="fas fa-eye"></i></th>
+					</tr>
+				</thead>
+				<tbody id="tblProfiles">
+				</tbody>
+			</table>
 		</div>
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
