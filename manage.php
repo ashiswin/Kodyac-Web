217c5a085cee14c1c872df065f6e60a8542cef45
Isaac Ashwin <ashiswin@gmail.com>
2018-02-07 10:21:05 +0800

    Debugging chartjs

--- manage.php
+++ manage.php
@@ -79,44 +79,38 @@
 		var days = getDaysArray(2018, 2);
 		console.log(days);
 		
-		new Chart(document.getElementById("chrtKYCInMonth"), {
+		var color = Chart.helpers.color;
+		var config = {
 			type: 'line',
 			data: {
-				labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-				datasets: [{ 
-						data: [86,114,106,106,107,111,133,221,783,2478],
-						label: "Africa",
-						borderColor: "#3e95cd",
-						fill: false
-					}, { 
-						data: [282,350,411,502,635,809,947,1402,3700,5267],
-						label: "Asia",
-						borderColor: "#8e5ea2",
-						fill: false
-					}, { 
-						data: [168,170,178,190,203,276,408,547,675,734],
-						label: "Europe",
-						borderColor: "#3cba9f",
-						fill: false
-					}, { 
-						data: [40,20,10,16,24,38,74,167,508,784],
-						label: "Latin America",
-						borderColor: "#e8c3b9",
-						fill: false
-					}, { 
-						data: [6,3,2,2,7,26,82,172,312,433],
-						label: "North America",
-						borderColor: "#c45850",
-						fill: false
-					}
-				]
+				labels: days,
+				datasets: [{
+					label: "My First dataset",
+					backgroundColor: color("#ff0000").alpha(0.5).rgbString(),
+					borderColor: "#ff0000",
+					fill: false,
+					data: [
+						20,9,22,7,4,12,9,22,7,4,12,9,22,7,4,12,9,22,7,4,12,9,22,7,4,12,0,0
+					],
+				}, {
+					label: "My Second dataset",
+					backgroundColor: color("#0000ff").alpha(0.5).rgbString(),
+					borderColor: "#0000ff",
+					fill: false,
+					data: [
+						2,8,20,6,4,12,9,22,7,4,12,9,22,7,4,12,9,22,7,4,12,9,22,7,4,12,0,0
+					],
+				}]
 			},
 			options: {
-				title: {
-					display: true,
-					text: 'World population per region (in millions)'
+				title:{
+				    text: "Chart.js Time Scale"
 				}
 			}
-		});
+				
+		};
+		
+		var ctx = document.getElementById("chrtKYCInMonth").getContext("2d");
+		window.myLine = new Chart(ctx, config);
 	</script>
 </html>
