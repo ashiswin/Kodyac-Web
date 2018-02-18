646ab6f4a6ed0567961f113cb5ef8030fcc0ead6
Isaac Ashwin <ashiswin@gmail.com>
2018-02-18 16:36:41 +0800

    Added API Keys skeleton

---
+++ apikeys.php
@@ -0,0 +1,59 @@
+<?php
+	session_start();
+	
+	if(isset($_POST['companyId'])) {
+		$_SESSION['companyId'] = $_POST['companyId'];
+	}
+	if(!isset($_SESSION['companyId'])) {
+		header("Location: /");
+	}
+	
+	require_once 'scripts/utils/database.php';
+	require_once 'scripts/connectors/CompanyConnector.php';
+	require_once 'scripts/connectors/LinkConnector.php';
+	
+	$CompanyConnector = new CompanyConnector($conn);
+	$LinkConnector = new LinkConnector($conn);
+	
+	$company = $CompanyConnector->select($_SESSION['companyId']);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>KodYaC - API Keys</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+		<style type="text/css">
+			html, body {
+				margin: 0;
+				padding: 0;
+			}
+			@font-face {
+				font-family: "Martel";
+				src: url("font/martel-regular.otf") format("opentype");
+			}
+			@font-face {
+				font-family: "Ubuntu Bold";
+				src: url("font/Ubuntu-B.ttf") format("truetype");
+			}
+		</style>
+	</head>
+	<body>
+		<?php require_once 'nav.php' ?>
+		<div class="bg-inverse">
+			<h1 style="font-family: 'Martel', Times New Roman, serif; font-weight: bold; text-align: center; padding: 5vh; color: #FFFFFF; font-size: 3em">API Keys</h1>
+		</div>
+		<div class="container">
+			
+		</div>
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+		<script type="text/javascript">
+			$("#navAPIKeys").addClass('active');
+			
+		</script>
+	</body>
+</html>
