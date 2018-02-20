9aed0a7661117e7c3820c8541f9980dd938ed711
Isaac Ashwin <ashiswin@gmail.com>
2018-02-21 01:07:29 +0800

    Added navbar

--- links/kyc.php
+++ links/kyc.php
@@ -40,6 +40,17 @@
 		</style>
 	</head>
 	<body>
+		<nav class="navbar navbar-default">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?php echo $_SERVER['PHP_SELF']; ?>">KodYaC</a>
+			</div>
+		</nav>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-6 offset-md-3 text-center">
--- links/kycmain.php
+++ links/kycmain.php
@@ -57,24 +57,45 @@
 		</style>
 	</head>
 	<body>
+		<!-- HTML file containing the navbar to reduce repetition of code -->
+		<nav class="navbar navbar-default">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="<?php echo $_SERVER['PHP_SELF']; ?>">KodYaC</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li id="navCreate"><a href="createevent.php">Create Event</a></li>
+					<li id="navManage"><a href="manageevents.php">Manage Events</a></li>
+					<li id="navUsers"><a href="users.php">Voters</a></li>
+					<li id="navAccount"><a href="account.php">Account</a></li>
+					<li id="navLogout"><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</nav>
 		<div class="container-fluid"><!-- To get it to take up the whole width -->
 			<div class="row main-content">
 				<!-- Create side pane with events -->
-				<div class="col-md-4 col-xs-4">
+				<div class="col-md-3 col-xs-3">
 					<div class="scrollable">
 						<table class="table table-hover">
 							<?php
 								$methods = explode("|", $company[CompanyConnector::$COLUMN_METHODS]);
 								
 								if(in_array("sms", $methods)) {
-									echo "<tr id=\"mtdSMS\"><td class=\"bg-danged\"><h4>SMS Verification</h4><div style=\"font-size: 12px; color: #AAAAAA;\">Status: Incomplete</div></td></tr>";
+									echo "<tr id=\"mtdSMS\"><td class=\"bg-danger\"><h4>SMS Verification</h4><div style=\"font-size: 12px; color: #AAAAAA;\">Status: Incomplete</div></td></tr>";
 								}
 							?>
 						</table>
 					</div>
 				</div>
 				<!-- Begin main detail view -->
-				<div class="col-md-8 col-xs-8 scrollable" style="border-left: 1px solid #CCCCCC;">
+				<div class="col-md-9 col-xs-9 scrollable" style="border-left: 1px solid #CCCCCC;">
 					<!-- Begin Details panel -->
 					<div id="detailsPane">
 					</div>
