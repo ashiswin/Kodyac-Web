0c127a66b1e9f92c37d0ec2c658dfe1003ab4da9
Isaac Ashwin <ashiswin@gmail.com>
2018-02-21 10:26:45 +0800

    Prevent form submission

--- links/kycmain.php
+++ links/kycmain.php
@@ -114,14 +114,14 @@
 					<div class="detail-pane" id="SMSPane">
 						<h1 style="margin-top: 2vh">SMS Verification</h1>
 						<br>
-						<form class="form form-inline">
+						<div class="form form-inline">
 							<div class="form-group">
 								<select id="slcCountryCode" class="form-control"><?php echo file_get_contents('countrycodes.txt'); ?></select>
 								<input type="text" placeholder="Phone number" class="form-control" id="txtNumber" />
 								<button class="btn btn-primary" id="btnSendSMS">Send SMS</button>
 								<button class="btn btn-secondary" id="btnResendSMS">Resend SMS</button>
 							</div>
-						</form>
+						</div>
 						<br>
 						<form class="form form-inline">
 							<div class="form-group">
