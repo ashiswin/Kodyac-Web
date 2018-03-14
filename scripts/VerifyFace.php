dc90ec21c762f11aa955aa808c9806ea33170fcc
Isaac Ashwin <ashiswin@gmail.com>
2018-03-14 11:47:59 +0800

    Testing verify

--- scripts/VerifyFace.php
+++ scripts/VerifyFace.php
@@ -67,6 +67,28 @@
 			break;
 		}
 	}
-	var_dump($faceId1);
-	var_dump($faceId2);
+	
+	$request = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/verify';
+
+	$headers = array(
+	    // Request headers
+	    'Content-Type: application/json',
+
+	    // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
+	    'Ocp-Apim-Subscription-Key: eeee342e022e4bcf99e30fe84d00efa5',
+	    //'Ocp-Apim-Subscription-Key' => ' fa9a97b26e3547b09aa26eff259fa0bb',
+	);
+	
+	$s = curl_init(); 
+
+	curl_setopt($s, CURLOPT_URL, $request);
+	curl_setopt($s, CURLOPT_HEADER, TRUE);
+	curl_setopt($s, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($s, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($s, CURLOPT_RETURNTRANSFER, true); 
+	curl_setopt($s, CURLOPT_POSTFIELDS, '{"faceId1": "' . $faceId1[0]["faceId"] . '", "faceId2": "' . $faceId2[0]["faceId"] . '"}');
+	
+	$result = curl_exec($s);
+	
+	echo($result);
 ?>
