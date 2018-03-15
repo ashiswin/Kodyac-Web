f7b68c9f16e18b86bdd21d2d80bdc5f005e8445f
Isaac Ashwin <ashiswin@gmail.com>
2018-03-15 22:35:26 +0800

    Bugfix: address needs concatenation

--- scripts/GetMyInfo.php
+++ scripts/GetMyInfo.php
@@ -28,7 +28,7 @@
 	$response["details"]["sex"] = $result->sex->value;
 	$response["details"]["race"] = prettyRace($result->race->value);
 	$response["details"]["dob"] = $result->dob->value;
-	$response["details"]["address"] = $result->regadd->value;
+	$response["details"]["address"] = $result->regadd->block . " " . $result->regadd->street . " #" . $result->regadd->floor . "-" . $result->regadd->unit . ", S" . $result->regadd->postal;
 	
 	echo(json_encode($response));
 ?>
