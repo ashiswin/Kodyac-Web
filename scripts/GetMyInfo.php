06c32ded365cea78426cf2ab77b25c9dbb97fc86
Isaac Ashwin <ashiswin@gmail.com>
2018-03-15 22:24:02 +0800

    Test decoded output

--- scripts/GetMyInfo.php
+++ scripts/GetMyInfo.php
@@ -6,7 +6,6 @@
 	curl_setopt($s, CURLOPT_URL, "https://myinfo.api.gov.sg/dev/L0/v1/person/" . $nric . "/");
 	curl_setopt($s, CURLOPT_RETURNTRANSFER, true); 
 	
-	$result = curl_exec($s);
-	
-	echo $result;
+	$result = json_decode(curl_exec($s));
+	var_dump($result);
 ?>
