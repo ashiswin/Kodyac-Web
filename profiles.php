4142510ba019d3e0cd092f75f56da8d096cd51c6
Isaac Ashwin <ashiswin@gmail.com>
2018-03-21 16:42:12 +0800

    Changed to modal large

--- profiles.php
+++ profiles.php
@@ -92,8 +92,8 @@
 				</tbody>
 			</table>
 		</div>
-		<div class="modal modal-lg fade" id="mdlViewProfile">
-			<div class="modal-dialog" role="document">
+		<div class="modal fade" id="mdlViewProfile">
+			<div class="modal-dialog modal-lg" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
 						<h5 class="modal-title">View Profile</h5>
